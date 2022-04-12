c4a2f2b5527336336ab400fceef6f118d69c5a0a
Matthew Gilpin <s4580160@student.uq.edu.au>
2022-04-12 20:45:51 +1000

    Added code to read adc values

--- battery_monitor/measurements.c
+++ battery_monitor/measurements.c
@@ -6,6 +6,53 @@ void measurements_init(BatteryMonitConfig *bmc) {
     adc_init();
 
     // Work out the pins to init
-    bmc->adcs_in_use;
+    // ADC pins are from GPIO26 to GPIO39
+    for (int i = 0; i < 4; i++ {
 
+        // check if bit is set
+        if (!!(bmc->adc_in_use & (1 << i)) {
+
+            // 26 pin offset.
+            adc_gpio_init(i + 26);
+        }
+    }
+
+    adc_select_input(0)
+
+}
+
+void take_measurements(BatteryMonitConfig *bmc, Measurements *meas) {
+
+    // Normally we'd have a conversion factor here, but since we are 
+    // transmitting raw values and computing the voltages on the 
+    // reciever side, we'll omit this step here
+    // const float conversion_factor = 3.3f / (1 << 12);
+
+    for (int i = 0; i < 4; i++ {
+
+        // check if bit is set
+        if (!!(bmc->adc_in_use & (1 << i)) {
+
+            adc_select_input(i);
+
+            uint16_t result = 
+
+            switch (i) {
+                case 0:
+                    meas->a0 = adc_read();
+                    break;
+                case 1:
+                    meas->a1 = adc_read();
+                    break;
+                case 2:
+                    meas->a2 = adc_read();
+                    break;
+                case 3:
+                    meas->a3 = adc_read();
+                    break;
+            }
+
+
+        }
+    }
 }
--- battery_monitor/measurements.h
+++ battery_monitor/measurements.h
@@ -4,6 +4,13 @@
 #include "hardware/adc.h"
 #include "setup.h"
 
+typedef struct {
+    uint16_t a0;
+    uint16_t a1;
+    uint16_t a2;
+    uint16_t a3;
+} Measurements;
+
 void measurements_init(BatteryMonitConfig *bmc);
 
 #endif
